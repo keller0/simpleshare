c4ddf3dceaeff7b9ef03592a853aacffd302f6c6
caijiaqi <caijiaqi2017@gmail.com>
2024-12-16 01:10:48 +0800

    clean up code, do real delete when click delete, update style set content width

--- http.go
+++ http.go
@@ -15,16 +15,17 @@ import (
 //go:embed web
 var webDir embed.FS
 
+// tmpFile struct
 type tmpFile struct {
-	T  string `json:"t"`
-	N  string `json:"n"`
-	Ns string `json:"ns"`
+	T  string `json:"t"`  // 0: normal file
+	N  string `json:"n"`  // file url
+	Ns string `json:"ns"` // file name
 }
 
 var (
 	sData    string
-	imgList  []string
-	fileList []tmpFile
+	imgList  []string  // img url list
+	fileList []tmpFile // file list
 )
 
 func runServer() error {
@@ -37,13 +38,13 @@ func runServer() error {
 
 	setRouter(r)
 
-	// Set memory limit for multipart forms (default is 32 MiB)
-	r.MaxMultipartMemory = 50 << 20
+	loadOldFiles()
+
+	// Set memory limit for multipart forms
+	r.MaxMultipartMemory = 20 << 20 // 20 MiB
 
 	ads := address + ":" + port
 	fmt.Println("server started at http://" + ads)
-
-	loadOldFiles()
 
 	return r.Run(ads)
 
@@ -56,19 +57,21 @@ func setRouter(r *gin.Engine) {
 
 	r.StaticFS("/static", http.FS(webDir))
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imaList": imgList, "fileList": fileList})
+		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imgList": imgList, "fileList": fileList})
 	})
 
 	r.POST("/", func(c *gin.Context) {
 		sData = c.PostForm("sData")
-		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imaList": imgList, "fileList": fileList})
+		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imgList": imgList, "fileList": fileList})
 	})
 
 	r.POST("/clearFile", func(c *gin.Context) {
+		clearTmpFile()
 		fileList = nil
 		imgList = nil
-		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imaList": imgList, "fileList": fileList})
+		c.HTML(http.StatusOK, "index.html", gin.H{"data": sData, "imgList": imgList, "fileList": fileList})
 	})
+
 	r.POST("/upload", func(c *gin.Context) {
 		// Single file
 		file, err := c.FormFile("file")
@@ -77,7 +80,6 @@ func setRouter(r *gin.Engine) {
 			c.String(http.StatusBadRequest, err.Error())
 		}
 
-		// saveName := strconv.FormatInt(time.Now().UnixNano(), 10) + path.Ext(file.Filename)
 		saveName := file.Filename
 
 		err = c.SaveUploadedFile(file, tmpFileDir+"/"+saveName)
@@ -119,7 +121,17 @@ func loadOldFiles() {
 			fileList = append(fileList, tmpFile{T: "0", N: fileUrl, Ns: f.Name()})
 		}
 	}
+}
 
+func clearTmpFile() {
+	err := os.RemoveAll(tmpFileDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.MkdirAll(tmpFileDir, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func isImgSimple(name string) bool {
